0ab11ca52f6a4c02950e6aac3a698768ed38f915
brenekv.03 <brenekv.03@A321-15>
2022-11-24 10:54:18 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> list = new List< int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            Random random = new Random();
+            for(int i = 0;i<n;i++)
+            {
+                list.Add(random.Next(-1000,999));
+            }
+            foreach(int i in list)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int max = -1001;
+            int druheMax = -1001;
+            int poradiMax = 0;
+            int poradiDruhehoMax = 0;
+            for(int i =0;i<list.Count;i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                    poradiMax = i;
+                }
+                if (list[i] > druheMax && list[i] < max)
+                {
+                    druheMax = list[i];
+                    poradiDruhehoMax=i;
+                }
+            }
+            MessageBox.Show("Max: " + max + " Poradi max: " + poradiMax + "\nDruhe max: " + druheMax + " Poradi druheho max: " + poradiDruhehoMax);
+        }
+    }
+}
